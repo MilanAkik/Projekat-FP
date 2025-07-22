3faabc88b7e6571ae57aaaf518a18c69bcd7baa7
MilanAkik <mixa.mixony@gmail.com>
2025-07-22 23:03:49 +0200

    Added some more UI with more generalization

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,25 +1,26 @@
-import scala.swing._
-import scala.swing.event._
+import scala.swing.{Alignment, *}
+import scala.swing.event.*
 
 object Main extends SimpleSwingApplication {
-  def top = new MainFrame {
-    title = "Hello Scala Swing"
+  def top: MainFrame = new MainFrame {
+    title = "Minesweeper"
     preferredSize = new Dimension(300, 200)
-
-    val label = new Label("Click the button")
-    val button = new Button("Click me")
+    val label = new Label("Minesweeper")
+    val buttonPlay = new Button("Play")
+    val buttonQuit = new Button("Quit")
+    val buttons: List[Button] = List(buttonPlay, buttonQuit)
 
     contents = new BoxPanel(Orientation.Vertical) {
       contents += label
-      contents += button
+      for(button <- buttons) contents += button
       border = Swing.EmptyBorder(10, 10, 10, 10)
     }
 
-    listenTo(button)
+    for(button <- buttons) listenTo(button)
 
     reactions += {
-      case ButtonClicked(`button`) =>
-        label.text = "Button clicked!"
+      case ButtonClicked(`buttonPlay`) => label.text = "Now we are playing!"
+      case ButtonClicked(`buttonQuit`) => label.text = "Quit it!"
     }
   }
 }
