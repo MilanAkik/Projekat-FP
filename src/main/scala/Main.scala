153531b1406f336ca87537c6a47377be260f45c6
MilanAkik <mixa.mixony@gmail.com>
2025-07-22 23:44:37 +0200

    Setup better formatting and added the edit button

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,23 +2,42 @@ import scala.swing.{Alignment, *}
 import scala.swing.event.*
 
 object Main extends SimpleSwingApplication {
+
+  val customFont = new Font("Arial", 1000, 32)
+
   def top: MainFrame = new MainFrame {
-    val label = new Label("Minolovac")
-    val buttonPlay = new Button("Igraj")
-    val buttonQuit = new Button("Izadji")
-    val buttons: List[Button] = List(buttonPlay, buttonQuit)
+    val label:Label = new Label("Minolovac"){
+      font = customFont
+      override def horizontalAlignment: Alignment.Value = Alignment.Center
+    }
+    val buttonPlay:Button = new Button("Igraj"){
+      font = customFont
+      override def horizontalAlignment: Alignment.Value = Alignment.Center
+      margin = new Insets(10,10,10,10)
+    }
+    val buttonEdit:Button = new Button("Izmeni nivo") {
+      font = customFont
+      override def horizontalAlignment: Alignment.Value = Alignment.Center
+      margin = new Insets(10, 10, 10, 10)
+    }
+    val buttonQuit:Button = new Button("Izadji"){
+      font = customFont
+      override def horizontalAlignment: Alignment.Value = Alignment.Center
+    }
+    val buttons: List[Button] = List(buttonPlay, buttonEdit, buttonQuit)
 
     title = "Minolovac"
-    contents = new BoxPanel(Orientation.Vertical) {
+    contents = new GridPanel(4,1) {
       contents += label
       for(button <- buttons) contents += button
-      border = Swing.EmptyBorder(10, 10, 10, 10)
+      border = Swing.EmptyBorder(10, 100, 10, 100)
+      vGap = 20
     }
-
     for(button <- buttons) listenTo(button)
 
     reactions += {
       case ButtonClicked(`buttonPlay`) => label.text = "Now we are playing!"
+      case ButtonClicked(`buttonEdit`) => label.text = "We are still editing!"
       case ButtonClicked(`buttonQuit`) => label.text = "Quit it!"
     }
   }
