50300d052787d653f0e61f5bebe06d8efae93cba
MilanAkik <mixa.mixony@gmail.com>
2025-08-16 22:01:01 +0200

    Added a button for showing the high score

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,11 +19,12 @@ object Main extends SimpleSwingApplication {
     val label:Label = new MenuLabel("Minolovac")
     val buttonPlay:Button = new MenuButton("Igraj")
     val buttonEdit:Button = new MenuButton("Izmeni nivo")
-    val buttonQuit:Button = new MenuButton("Izadji")
-    val elements: List[Component] = List(label, buttonPlay, buttonEdit, buttonQuit)
+    val buttonHighScore:Button = new MenuButton("Najbolji rezultati")
+    val buttonQuit: Button = new MenuButton("Izadji")
+    val elements: List[Component] = List(label, buttonPlay, buttonEdit, buttonHighScore, buttonQuit)
 
     title = "Minolovac"
-    contents = new GridPanel(4,1) {
+    contents = new GridPanel(5,1) {
       for(element <- elements) contents += element
       border = Swing.EmptyBorder(20, 100, 40, 100)
       vGap = 20
@@ -35,6 +36,7 @@ object Main extends SimpleSwingApplication {
         DifficultyChooser.frame.visible = true
         DifficultyChooser.frame.centerOnScreen()
       case ButtonClicked(`buttonEdit`) => label.text = "We are still editing!"
+      case ButtonClicked(`buttonHighScore`) => label.text = "No high score!"
       case ButtonClicked(`buttonQuit`) => this.close()
     }
   }
