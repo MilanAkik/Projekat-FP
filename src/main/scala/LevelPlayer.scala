b35499c577ca40354b391107d85c431a53699710
MilanAkik <mixa.mixony@gmail.com>
2025-08-17 22:24:30 +0200

    Added a toolbar and grid making methods

--- src/main/scala/LevelPlayer.scala
+++ src/main/scala/LevelPlayer.scala
@@ -1,14 +1,33 @@
-import scala.swing.{Component, Frame, GridPanel, Swing}
+import scala.swing.{Button, Component, Frame, GridPanel, Label, Orientation, Swing, ToolBar}
 
 object LevelPlayer {
 
+  val btnSave = new Button("Sacuvaj")
+  val btnHint = new Button("Pomoc")
+  val labelScore = new Label("Rezultat: ")
+  val labelTime = new Label("Vreme: ")
+
+  def makeToolbar():GridPanel = {
+    new GridPanel(1, 4) {
+      val elements: List[Component] = List(btnSave, btnHint, labelScore, labelTime)
+      for (element <- elements) contents += element
+    }
+  }
+
+  def makeGrid(w: Int, h: Int):GridPanel = {
+    new GridPanel(h,w){
+      val elements: List[Component] = List()
+      for (i <- 1 to w; j <- 1 to h) contents += new Button("("+i+","+j+")")
+    }
+  }
+
   def makeFrame(difficulty: String) : Unit = {
     frame = new Frame() {
-      val elements: List[Component] = List()
+      val elements: List[Component] = List(makeToolbar(),makeGrid(5,10))
       title = "Igrajte " + difficulty
-      contents = new GridPanel(3, 4) {
+      contents = new GridPanel(2, 1) {
         for (element <- elements) contents += element
-        border = Swing.EmptyBorder(20, 400, 40, 400)
+        border = Swing.EmptyBorder(20, 100, 40, 100)
         vGap = 20
       }
     }
