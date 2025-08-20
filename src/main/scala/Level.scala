63bc93879716459ad5103a8ef78f11036a25049a
MilanAkik <mixa.mixony@gmail.com>
2025-08-20 23:48:43 +0200

    Added random level generation

--- src/main/scala/Level.scala
+++ src/main/scala/Level.scala
@@ -1,8 +1,23 @@
-class Level(var path: String) {
+import scala.util.Random
 
-  val matrix: Array[Array[Boolean]] = readInMatrix(path)
+class Level {
+
+  var matrix: Array[Array[Boolean]] = new Array[Array[Boolean]](1)
+
+  val random: Random = new Random(1234)
   def Width(): Int = matrix.length
   def Height(): Int = matrix(0).length
+
+  def this(path: String)={
+    this()
+    matrix = readInMatrix(path)
+  }
+
+  def this(width:Int, height:Int, mines: Int) = {
+    this()
+    matrix = randomLevel(width, height, mines)
+  }
+
 
   private def readInMatrix(path: String):Array[Array[Boolean]] = {
     val source = scala.io.Source.fromFile(path)
@@ -16,6 +31,27 @@ class Level(var path: String) {
       for(j <- 0 until cols){
         res(i)(j) = lines(i)(j)=='#'
       }
+    }
+    res
+  }
+
+  private def randomLevel(width:Int, height:Int, mines: Int): Array[Array[Boolean]] = {
+    val res = new Array[Array[Boolean]](height)
+    for (i <- 0 until height) {
+      res(i) = new Array[Boolean](width)
+      for (j <- 0 until width) {
+        res(i)(j) = false
+      }
+    }
+    for (i <- 0 until mines){
+      var col: Int = random.nextInt(width)
+      var row: Int = random.nextInt(height)
+      while
+        res(row)(col)
+      do
+        col = random.nextInt(width)
+        row = random.nextInt(height)
+      res(row)(col)=true
     }
     res
   }
--- src/main/scala/LevelPlayer.scala
+++ src/main/scala/LevelPlayer.scala
@@ -42,9 +42,9 @@ object LevelPlayer {
     }
     val w: Int = board.level.Width()
     val h: Int = board.level.Height()
-    new GridPanel(h,w){
+    new GridPanel(w,h){
       var elements: List[Component] = List()
-      for (i <- 0 until w; j <- 0 until h) elements = elements :+ makeGridButton(i,j,board.matrix(i)(j))
+      for (i <- 0 until w; j <- 0 until h) elements = elements :+ makeGridButton(i,j,board.matrix(j)(i))
       for (element <- elements) {
         contents += element
         listenTo(element.mouse.clicks)
@@ -58,7 +58,12 @@ object LevelPlayer {
 
   def makeFrame(difficulty: String, fileName: String) : Unit = {
     frame = new Frame() {
-      val level = new Level(Constants.levelPaths+"\\"+difficulty+"\\"+fileName)
+      val level: Level = fileName match
+        case "random" => difficulty match
+          case "easy" => new Level(9,9,10)
+          case "medium" => new Level(16,16,40)
+          case "hard" =>new Level(30,16,99)
+        case _ => new Level(Constants.levelPaths+"\\"+difficulty+"\\"+fileName)
       val board:Board = new Board(level)
       val elements: List[Component] = List( makeToolbar(), Swing.VStrut(10), makeGrid(board) )
       title = "Igrajte " + difficulty
