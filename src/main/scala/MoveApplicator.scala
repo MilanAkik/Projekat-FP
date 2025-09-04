e22ae5591ebde88a9c6d5f5810618226799b1e59
MilanAkik <mixa.mixony@gmail.com>
2025-09-05 00:33:16 +0200

    Added an implementation of the right click and an empty left click with an exception for any other move

--- src/main/scala/MoveApplicator.scala
+++ src/main/scala/MoveApplicator.scala
@@ -1,5 +1,20 @@
 object MoveApplicator {
   def ApplyMoves(board: Board, moves:List[Move]): Board = {
+    val Move(button,x,y) = moves.head
+    var rest = moves.tail
+    button match {
+      case 'L' => 1
+      case 'R' => {
+        val oldVal = board.matrix(x)(y)
+        val newVal = oldVal match {
+          case FieldState.Unopened => FieldState.Flag
+          case FieldState.Flag => FieldState.Unopened
+          case _ => oldVal
+        }
+        board.matrix(x)(y) = newVal
+      }
+      case _ => throw new IllegalArgumentException("Unexpected number: "+move.button)
+    }
     board
   }
 }
