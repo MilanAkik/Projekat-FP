8767f4620546ee8b78daeae3901abc17c30fef18
MilanAkik <mixa.mixony@gmail.com>
2025-09-05 22:42:56 +0200

    Added the tail recursive return at the end of the method

--- src/main/scala/MoveApplicator.scala
+++ src/main/scala/MoveApplicator.scala
@@ -1,4 +1,7 @@
+import scala.annotation.tailrec
+
 object MoveApplicator {
+  @tailrec
   def ApplyMoves(board: Board, moves:List[Move]): Board = {
     val Move(button,x,y) = moves.head
     var rest = moves.tail
@@ -8,16 +11,16 @@ object MoveApplicator {
         oldVal
       }
       case 'R' => {
-        val newVal = oldVal match {
+        oldVal match {
           case FieldState.Unopened => FieldState.Flag
           case FieldState.Flag => FieldState.Unopened
           case _ => oldVal
         }
-        newVal
       }
       case _ => throw new IllegalArgumentException("Unexpected number: " + button)
     }
     board.matrix(x)(y) = newVal
-    board
+    if (rest.isEmpty) board
+    else ApplyMoves(board, rest)
   }
 }
