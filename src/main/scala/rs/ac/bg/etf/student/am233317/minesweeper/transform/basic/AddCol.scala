edabaa0a4d3b3e70b3f4664099a52d5869453480
MilanAkik <mixa.mixony@gmail.com>
2025-09-13 21:06:04 +0200

    Added a add col transform that adds a column to the left or right of the level depending on the left flag

---
+++ src/main/scala/rs/ac/bg/etf/student/am233317/minesweeper/transform/basic/AddCol.scala
@@ -0,0 +1,19 @@
+package rs.ac.bg.etf.student.am233317.minesweeper.transform.basic
+
+import rs.ac.bg.etf.student.am233317.minesweeper.model.Level
+import rs.ac.bg.etf.student.am233317.minesweeper.transform.{Error, Transform}
+
+class AddCol (val left: Boolean) extends Transform {
+  override def apply(level: Level, args: Array[Int]): Either[Level, Error] = {
+    val name = if(left) "left" else "right"
+    if(args.length>0) Right(Error(s"add_col_$name does not accept any arguments"))
+    else {
+      val w = level.Width()
+      val h = level.Height()
+      val res: Array[Array[Boolean]] = Array.fill(h, w+1)(false)
+      val dx = if(left) 1 else 0
+      for (i<- 0 until h; j <- 0 until w) res(i)(j+dx) = level.matrix(i)(j)
+      Left(new Level(res))
+    }
+  }
+}
