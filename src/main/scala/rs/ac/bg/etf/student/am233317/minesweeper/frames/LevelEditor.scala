35c96d03a3247cc0be0548410320bb1e9375e446
MilanAkik <mixa.mixony@gmail.com>
2025-09-15 21:18:32 +0200

    Added the application of one or more transforms to the level in the editor on a button click

--- src/main/scala/rs/ac/bg/etf/student/am233317/minesweeper/frames/LevelEditor.scala
+++ src/main/scala/rs/ac/bg/etf/student/am233317/minesweeper/frames/LevelEditor.scala
@@ -2,18 +2,21 @@ package rs.ac.bg.etf.student.am233317.minesweeper.frames
 
 import rs.ac.bg.etf.student.am233317.minesweeper.frames.LevelPlayer.{btnHint, btnMoves, btnSave, handleToolbarClick, labelScore, labelTime}
 import rs.ac.bg.etf.student.am233317.minesweeper.model.Level
+import rs.ac.bg.etf.student.am233317.minesweeper.transform.{Error, Transform}
 import rs.ac.bg.etf.student.am233317.minesweeper.ui.{GridButton, MenuButton, TransformField}
-import rs.ac.bg.etf.student.am233317.minesweeper.utility.Constants
+import rs.ac.bg.etf.student.am233317.minesweeper.utility.{CompositeParser, Constants}
+
 import java.io.File
 import java.nio.charset.StandardCharsets.UTF_8
 import java.nio.file.{Files, Path, Paths}
+import scala.annotation.tailrec
 import scala.swing.{BoxPanel, Button, Component, Dimension, FileChooser, FlowPanel, Frame, GridPanel, Orientation, Swing}
 import scala.swing.event.MouseClicked
 import scala.swing.event.{ButtonClicked, MouseClicked, WindowClosing}
 
 object LevelEditor {
 
-  private def handleToolbarClick(click: MouseClicked)(using level: Level): Unit = {
+  private def handleToolbarClick(click: MouseClicked): Unit = {
     click.source.name match {
       case "btnLoadComposite" =>
         val chooser = new FileChooser(new File(Constants.compositesPaths))
@@ -22,7 +25,21 @@ object LevelEditor {
           val linesString = try source.mkString finally source.close()
           txtTransforms.text = linesString
         }
-      case "btnApplyComposite" => println("btnApplyComposite")
+      case "btnApplyComposite" =>
+        val transforms = CompositeParser.parseComposite(txtTransforms.text)
+        transformApply(currentLevel,transforms) match
+          case Right(lvl) => currentLevel = lvl
+          case Left(err) => println(err.Message)
+        val gp = makeGrid()
+        boxPanel.contents.clear()
+        boxPanel.contents += gp
+        gp.peer.revalidate()
+        gp.peer.repaint()
+        boxPanel.peer.revalidate()
+        boxPanel.peer.repaint()
+        frame.peer.revalidate()
+        frame.peer.repaint()
+        frame.pack()
       case "btnSaveComposite" =>
         val chooser = new FileChooser(new File(Constants.compositesPaths))
         if (chooser.showSaveDialog(null) == FileChooser.Result.Approve) {
@@ -32,13 +49,26 @@ object LevelEditor {
       case "btnSaveLevel" =>
         val chooser = new FileChooser(new File(Constants.levelPaths))
         if (chooser.showSaveDialog(null) == FileChooser.Result.Approve) {
-          val text = level.matrix.map(x=>x.map(b=>if(b) '#' else '-').mkString).mkString("\n")
+          val text = currentLevel.matrix.map(x=>x.map(b=>if(b) '#' else '-').mkString).mkString("\n")
           Files.write(chooser.selectedFile.toPath, text.getBytes(UTF_8))
         }
     }
   }
 
-  private def makeButtons()(using level: Level): BoxPanel = new BoxPanel(Orientation.Vertical) {
+  @tailrec()
+  private def transformApply(l: Level, t: List[(Transform, Array[Int])]) : Either[Error, Level] = {
+    val head = t.head
+    val tail = t.tail
+    val tailEmpty = tail.isEmpty
+    val (tr, args) = head
+    val res = tr(l, args)
+    (tailEmpty, res) match
+      case (true,Right(lvl)) => Right(lvl)
+      case (false,Right(lvl)) => transformApply(lvl, tail)
+      case (_, Left(err)) => Left(err)
+  }
+
+  private def makeButtons(): BoxPanel = new BoxPanel(Orientation.Vertical) {
     val elements: List[Component] = List(btnLoadComposite, btnApplyComposite, btnSaveComposite, btnSaveLevel)
     for (element <- elements) {
       contents += element
@@ -47,17 +77,17 @@ object LevelEditor {
     reactions += { case click: MouseClicked => handleToolbarClick(click) }
   }
 
-  private def makeUpperPanel()(using level: Level): FlowPanel = new FlowPanel(){
+  private def makeUpperPanel(): FlowPanel = new FlowPanel(){
     val elements: List[Component] = List(txtTransforms, makeButtons())
     for (element <- elements) contents += element
   }
-  private def makeGrid()(using level: Level): GridPanel = {
-    val w: Int = level.Width()
-    val h: Int = level.Height()
-    grid = List()
+  private def makeGrid(): GridPanel = {
+    val w: Int = currentLevel.Width()
+    val h: Int = currentLevel.Height()
+    var grid = List[Component]()
     new GridPanel(h, w) {
       for (j <- 0 until h; i <- 0 until w)
-        grid = grid :+ new GridButton(if(level.matrix(j)(i)) "@" else " ")
+        grid = grid :+ new GridButton(if(currentLevel.matrix(j)(i)) "@" else " ")
       for (element <- grid) { contents += element }
     }
   }
@@ -65,20 +95,23 @@ object LevelEditor {
   def makeFrame(level: Level): Unit = {
     currentLevel = level
     frame = new Frame() {
-      given Level = currentLevel
-      val elements: List[Component] = List( makeUpperPanel(), Swing.VStrut(10), makeGrid())
+      boxPanel = new BoxPanel(Orientation.Vertical){
+        contents += makeGrid()
+      }
+      val elements: List[Component] = List( makeUpperPanel(), Swing.VStrut(10), boxPanel)
       contents = new BoxPanel(Orientation.Vertical) {
         for (element <- elements) contents += element
         border = Swing.EmptyBorder(10, 10, 10, 10)
       }
-      minimumSize = new Dimension(1024, 768)
+//      minimumSize = new Dimension(1024, 768)
     }
     frame.visible = true
     frame.centerOnScreen()
   }
 
   private var currentLevel: Level = _
-  private var grid: List[Button] = List()
+  private var gridPanel: GridPanel = _
+  private var boxPanel: BoxPanel = _
   private var frame: Frame = _
   private val txtTransforms: TransformField = new TransformField()
   private val btnLoadComposite: Button = new MenuButton("Ucitaj kompozit") { name = "btnLoadComposite" }
